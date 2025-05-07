d6509437d934205eeeec6fdd8af1403ac553825b
gastonpesoa <gastonpesoa@gmail.com>
2025-05-07 10:24:53 -0300

    Configurated BenchmarkDotNet nuget

---
+++ QuDeveloperChallenge.Console/WordFinderBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using QuDeveloperChallenge.Library;
+
+namespace QuDeveloperChallenge.Console
+{
+    [MemoryDiagnoser]
+    public class WordFinderBenchmarks
+    {
+        private readonly Consumer _consumer = new();
+        private WordFinder _wordFinder;
+
+        public WordFinderBenchmarks()
+        {
+            HashSet<string> matrix = [];
+            _wordFinder = new WordFinder(matrix);
+        }
+
+        [Benchmark]
+        public void RunFindBenchmark()
+        {
+            HashSet<string> wordStream = [];
+            _wordFinder.Find(wordStream).Consume(_consumer);
+        }
+    }
+}
