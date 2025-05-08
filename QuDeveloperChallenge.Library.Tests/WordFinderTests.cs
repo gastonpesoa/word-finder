b28e804ad8533f78059c65a7cf2ec332f5d21063
gastonpesoa <gastonpesoa@gmail.com>
2025-05-08 17:26:27 -0300

    Added stable version of finding one word in one row

--- QuDeveloperChallenge.Library.Tests/WordFinderTests.cs
+++ QuDeveloperChallenge.Library.Tests/WordFinderTests.cs
@@ -6,13 +6,60 @@ namespace QuDeveloperChallenge.Library.Tests
         public void Find_ShoulReturnCollectionOfStrings()
         {
             // Arrange
-            WordFinder wordFinder = new(new HashSet<string>());
+            string[] matrix = [];
+            WordFinder wordFinder = new(matrix);
+            string[] wordStream = [];
 
             // Act
-            IEnumerable<string> actual = wordFinder.Find(new HashSet<string>());
+            IEnumerable<string> actual = wordFinder.Find(wordStream);
             
             // Assert
             Assert.IsAssignableFrom<IEnumerable<string>>(actual);
+        }
+
+        [Fact]
+        public void Find_ShoulReturnEmtySetOfStringsIfNoWordsAreFound()
+        {
+            // Arrange
+            string[] matrix = ["chill"];
+            WordFinder wordFinder = new(matrix);
+            string[] wordStream = ["cold"];
+
+            // Act
+            IEnumerable<string> actual = wordFinder.Find(wordStream);
+
+            // Assert
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void Find_ShoulReturnWordFoundInOneRowMatrix()
+        {
+            // Arrange
+            string[] matrix = ["cold"];
+            WordFinder wordFinder = new(matrix);
+            string[] expected = ["cold"];
+
+            // Act
+            IEnumerable<string> actual = wordFinder.Find(expected);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Find_ShoulReturnWordFoundInOneRowMatrixContainingIt()
+        {
+            // Arrange
+            string[] matrix = ["abccoldwer"];
+            WordFinder wordFinder = new(matrix);
+            string[] expected = ["cold"];
+
+            // Act
+            IEnumerable<string> actual = wordFinder.Find(expected);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
--- QuDeveloperChallenge.Library/WordFinder.cs
+++ QuDeveloperChallenge.Library/WordFinder.cs
@@ -2,14 +2,46 @@
 {
     public class WordFinder : IWordFinder
     {
+        private readonly IEnumerable<string> _matrix;
+
         public WordFinder(IEnumerable<string> matrix)
         {
-
+            _matrix = matrix;
         }
 
         public IEnumerable<string> Find(IEnumerable<string> wordStream)
         {
-            return [];
+            HashSet<string> searchResults = [];
+
+            foreach (string word in wordStream)
+            {
+                foreach (string row in _matrix)
+                {
+                    for (int col = 0; col < row.Length; col++)
+                    {
+                        if (row[col] == word[0])
+                        {
+                            for (int charAt = 1; charAt < word.Length; charAt++)
+                            {
+                                if (row[col + charAt] == word[charAt])
+                                {
+                                    if (word.Length == charAt + 1)
+                                    {
+                                        searchResults.Add(word);
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return searchResults;
         }
     }
 }
