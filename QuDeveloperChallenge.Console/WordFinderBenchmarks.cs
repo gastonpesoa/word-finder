9813548e9938ba01422f07ea4d3a9ebc3f7c9007
gastonpesoa <gastonpesoa@gmail.com>
2025-05-13 22:53:12 -0300

    Added benchmarks

--- QuDeveloperChallenge.Console/WordFinderBenchmarks.cs
+++ QuDeveloperChallenge.Console/WordFinderBenchmarks.cs
@@ -1,4 +1,15 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿// * Summary *
+
+// BenchmarkDotNet v0.14.0, Windows 10 (10.0.19045.5737/22H2/2022Update)
+// AMD Ryzen 7 PRO 4750U with Radeon Graphics, 1 CPU, 16 logical and 8 physical cores
+// .NET SDK 9.0.203
+//
+//| Method                               | Mean      | Error    | StdDev   | Ratio | RatioSD | Gen0    | Allocated | Alloc Ratio |
+//| RunWordFinderBruteForceBenchmark     | 208.49 us | 4.093 us | 8.722 us | 1.00  | 0.06    | 1.7090  | 3.76 KB   | 1.00        |
+//| RunWordFinderRecursiveBenchmark      | 283.47 us | 5.630 us | 8.427 us | 1.36  | 0.07    | 1.4648  | 3.76 KB   | 1.00        |
+//| RunWordFinderTransposingBenchmark    | 55.09 us  | 1.053 us | 0.879 us | 0.26  | 0.01    | 11.1084 | 22.78 KB  | 6.06        |
+
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
 using QuDeveloperChallenge.Library;
 
@@ -94,7 +105,14 @@ namespace QuDeveloperChallenge.Console
         [Benchmark]
         public void RunWordFinderRecursiveBenchmark()
         {
-            WordFinderBruteForce wordFinder = new(_matrix);
+            WordFinderRecursive wordFinder = new(_matrix);
+            wordFinder.Find(_wordstream).Consume(_consumer);
+        }
+
+        [Benchmark]
+        public void RunWordFinderTransposingBenchmark()
+        {
+            WordFinderTransposing wordFinder = new(_matrix);
             wordFinder.Find(_wordstream).Consume(_consumer);
         }
     }
